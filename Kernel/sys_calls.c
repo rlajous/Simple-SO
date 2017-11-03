6ff028b1734bf58a620004878d3b29aaa87241c7
Unknown <rnavarro@itba.edu.ar>
2017-11-03 15:25:13 -0300

    mini Upgrade

--- Kernel/sys_calls.c
+++ Kernel/sys_calls.c
@@ -4,26 +4,27 @@
 extern int getSeconds();
 extern int getMinutes();
 extern int getHours();
-typedef void (*fn)(char* buffer, int size);
+typedef void (*fn)(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9);
 
 static fn sysCalls[3];
 
 
-void sys_clear(char* buffer, int size){
+void sys_clear(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
 	clear();
 }
 
-void sys_write(char* buffer, int size){
-	char* c=buffer;
+void sys_write(unsigned long buffer, unsigned long rcx, unsigned long r8, unsigned long r9){
+	char* c=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
 		printChar(*(c+i));
 	}
 }
-void sys_read(char* buffer, int size){
+void sys_read(unsigned long buffer, unsigned long size, unsigned long r8, unsigned long r9){
+	buff=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
-		*(buffer + i) =get_buffer();
+		*(buff + i) =get_buffer();
 	}
 }
 void load_sys(){
@@ -31,9 +32,9 @@ void load_sys(){
 	sysCalls[1]= &sys_write;
 	sysCalls[2]= &sys_write;
 }
-void sys_handler(int num,char*buffer,int size){
-	if (num>=0&&num<=3){
-		sysCalls[num](buffer,size);
+void sys_handler(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
+	if (rsi>=0&&rsi<=3){
+		sysCalls[rsi](rdx,rcx);
 	}
 	return;
 }
---
+++ Userland/SampleCodeModule/shell.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+
+
+void shell(){
+	int terminal = 1;
+	
+}
