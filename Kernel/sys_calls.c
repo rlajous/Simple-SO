b978e3fa21ee1dcaac3454df97a512ae6d05e890
Unknown <rnavarro@itba.edu.ar>
2017-11-03 15:44:03 -0300

    min fix

--- Kernel/sys_calls.c
+++ Kernel/sys_calls.c
@@ -4,26 +4,28 @@
 extern int getSeconds();
 extern int getMinutes();
 extern int getHours();
-typedef void (*fn)(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9);
+typedef void (*fn)(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10);
 
-static fn sysCalls[4];
+static fn sysCalls[5];
 
+void sys_time(unsigned long sec, unsigned long min, unsigned long hour, unsigned long month, unsigned long year){
 
-void sys_clear(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
+}
+void sys_clear(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
 	clear();
 }
 
-void sys_write(unsigned long buffer, unsigned long rcx, unsigned long r8, unsigned long r9){
+void sys_write(unsigned long buffer, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
 	char* c=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
 		printChar(*(c+i));
 	}
 }
-void sys_setTerminal(unsigned long size, unsigned long rcx, unsigned long r8, unsigned long r9){
+void sys_setTerminal(unsigned long size, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
 	setTerminal((int)size);
 }
-void sys_read(unsigned long buffer, unsigned long size, unsigned long r8, unsigned long r9){
+void sys_read(unsigned long buffer, unsigned long size, unsigned long r8, unsigned long r9, unsigned long r10){
 	buff=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
@@ -35,10 +37,11 @@ void load_sys(){
 	sysCalls[1]= &sys_write;
 	sysCalls[2]= &sys_write;
 	sysCalls[3]= &sys_setTerminal;
+	sysCalls[4]= &sys_time;
 }
-void sys_handler(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
+void sys_handler(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
 	if (rsi>=0&&rsi<=3){
-		sysCalls[rsi](rdx,rcx,r8,r9);
+		sysCalls[rsi](rdx,rcx,r8,r9,r10);
 	}
 	return;
 }
