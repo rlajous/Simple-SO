ef4e090f0accba5f76c3432b0d108a850021705c
Unknown <rnavarro@itba.edu.ar>
2017-11-03 17:24:11 -0300

    SYS fix

--- Kernel/sys_calls.c
+++ Kernel/sys_calls.c
@@ -5,28 +5,30 @@ extern int getSeconds();
 extern int getMinutes();
 extern int getHours();
 
-typedef void (*fn)(unsigned long rdi, unsigned long rsi, unsigned long rcx, unsigned long r8, unsigned long r9);
+typedef void (*fn)(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9);
 
 static fn sysCalls[5];
 
 void sys_time(unsigned long sec, unsigned long min, unsigned long hour, unsigned long month, unsigned long year){
-
+	sec=getSeconds();
+	min=getMinutes();
+	hour=getHours();
 }
-void sys_clear(unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
+void sys_clear(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
 	clear();
 }
 
-void sys_write(unsigned long buffer, unsigned long size, unsigned long r8, unsigned long r9, unsigned long r10){
+void sys_write(unsigned long buffer, unsigned long size, unsigned long rcx, unsigned long r8, unsigned long r9){
 	char* c=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
 		printChar(*(c+i));
 	}
 }
-void sys_setTerminal(unsigned long size, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
+void sys_setTerminal(unsigned long size, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
 	setTerminal((int)size);
 }
-void sys_read(unsigned long buffer, unsigned long size, unsigned long r8, unsigned long r9, unsigned long r10){
+void sys_read(unsigned long buffer, unsigned long size, unsigned long rcx, unsigned long r8, unsigned long r9){
 	char * buff=(char*)buffer;
 	for (int i = 0; i < size; i++)
 	{
@@ -40,9 +42,10 @@ void load_sys(){
 	sysCalls[3]= &sys_setTerminal;
 	sysCalls[4]= &sys_time;
 }
-void sys_handler(unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9, unsigned long r10){
+//rdi, rsi, rdx, rcx, r8 y r9
+void sys_handler(unsigned long rdi, unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9){
 	if (rsi>=0&&rsi<=3){
-		sysCalls[rsi](rdx,rcx,r8,r9,r10);
+		sysCalls[rdi](rsi,rdx,rcx,r8,r9);
 	}
 	return;
 }
--- Userland/SampleCodeModule/syscalls.c
+++ Userland/SampleCodeModule/syscalls.c
@@ -1,10 +1,13 @@
 //#include "syscalls.h"
 
-void _int80h(unsigned long rdi, unsigned long rsi, unsigned long rcx, unsigned long r8, unsigned long r9);
+void _int80h(unsigned long rdi, unsigned long rsi, unsigned long rdx, unsigned long rcx, unsigned long r8, unsigned long r9);
 
-void read(void *buffer, unsigned int size) {
-    return _int80h(2, (uint64_t) buffer, size,0,0);
+void read(char *buffer, unsigned int size) {
+     _int80h(2, (uint64_t) buffer, (uint64_t)size,0,0,0);
 }
-void write(void *buffer, unsigned int size) {
-    return _int80h(1, (uint64_t) buffer, size,0,0);
+void write(char *buffer, unsigned int size) {
+     _int80h(1, (uint64_t) buffer, (uint64_t)size,0,0,0);
+}
+void time(int sec,int min,int hs,int mont,int year) {
+     _int80h(4, (uint64_t) sec,(uint64_t) min,(uint64_t) hs, (uint64_t) mont,(uint64_t) year);
 }
