45936703f4fe3452b8c9d5672ed76daf1a4bb8d7
Sebastian Favaron <sfavaron@itba.edu.ar>
2017-11-06 01:56:23 +0000

    graph.c created online with Bitbucket

---
+++ Kernel/graph.c
@@ -0,0 +1,41 @@
+#include <keyboardDriver.h>
+#include <videoDriver.h>
+
+extern putPixel(x, y)
+
+//hay que reemplazar por los valores en videoDriver
+width = 1
+height = 1
+charWidth = 1
+charHeight = 1
+
+void graph_axis() {
+    for(int y=0; y<height-2*charHeight; y--) {
+    	putPixel(width/2, y);
+    }
+	for(int x=0; x<width; x++) {
+    	putPixel(x, (height-2*charHeight)/2);
+	}
+}
+
+void graph_line(int a, int b) {
+	for(int y=0; y<height-2*charHeight; y++) {
+		for(int x=0; x<width; x++) {
+			yAux = y+(height-2*charHeight)/2;
+			xAux = x-width/2;
+			if(yAux == xAux*a + b)
+				putPixel(x, y);
+		}
+    }
+}
+
+void graph_cuadratic(int a, int b, int c) {
+	for(int y=0; y<height-2*charHeight; y++) {
+		for(int x=0; x<width; x++) {
+			yAux = y+(height-2*charHeight)/2;
+			xAux = x-width/2;
+			if(yAux == xAux*xAux*a + xAux*b + c)
+				putPixel(x, y);
+		}
+    }
+}
